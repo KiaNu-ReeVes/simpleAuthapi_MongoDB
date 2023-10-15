3f91692d8b0500e6234f5866adaf13d7695821cb
KiaNu-ReeVes <kian.rabiei1387@gmail.com>
2023-10-15 15:03:22 +0330

    project MVC

---
+++ routes/routes.ts
@@ -0,0 +1,58 @@
+import express, { Request, Response } from 'express';
+import Food from '../models/food';
+
+const router = express.Router()
+
+router.post('/food', async (req: Request, res: Response) => {
+    const { name, amount, unit, calories, protein, carbs, fat } = req.body;
+    const food = new Food({
+        name,
+        amount,
+        unit,
+        calories,
+        protein,
+        carbs,
+        fat
+    });
+    const findFood = await Food.find({ name: name });
+    if (findFood[0]) return res.json("Duplicate Item")
+    const savedFood = await food.save();
+    res.json(savedFood);
+})
+
+router.get('/food', async (req: Request, res: Response) => {
+    const foods = await Food.find({});
+    res.json(foods);
+})
+
+router.get('/food/:id', async (req: Request, res: Response) => {
+    const food = await Food.findById(req.params.id);
+    res.json(food);
+})
+
+router.delete('/food/:id', async (req: Request, res: Response) => {
+    const food = await Food.findByIdAndDelete(req.params.id);
+    res.json(food);
+})
+
+router.put('/food/:id', async (req: Request, res: Response) => {
+    const { name, amount, unit, calories, protein, carbs, fat } = req.body;
+    const food = await Food.findByIdAndUpdate(req.params.id, {
+        name,
+        amount,
+        unit,
+        calories,
+        protein,
+        carbs,
+        fat
+    });
+    res.json(food);
+})
+
+router.post('/food/search', async (req: Request, res: Response) => {
+    const { name } = req.body;
+    const food = await Food.find({ name: { $regex: name, $options: 'i' } });
+    res.json(food);
+})
+
+export default router;
